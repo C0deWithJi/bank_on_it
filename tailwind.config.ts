840b0ae7d2c9239999c86dd149e18cd59ea9e865
C0deWithJi <codewithji@icloud.com>
2025-03-11 12:05:44 -0700

    updated font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -101,6 +101,10 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontSize: {
+        '16': '16px',
+        '14': '14px',
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
