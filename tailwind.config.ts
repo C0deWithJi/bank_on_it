a7f334e6ba5af1de78dc238b85d6c49a4557de34
C0deWithJi <codewithji@icloud.com>
2025-03-11 12:49:13 -0700

    updated placement of font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,10 @@ const config: Config = {
       },
     },
     extend: {
+      fontSize: {
+        '16': '16px',
+        '14': '14px',
+      },
       colors: {
         fill: {
           1: "rgba(255, 255, 255, 0.10)",
@@ -100,10 +104,6 @@ const config: Config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-      },
-      fontSize: {
-        '16': '16px',
-        '14': '14px',
       },
     },
   },
